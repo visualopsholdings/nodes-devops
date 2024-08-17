e2cd7cd299c499e86416bfce41876656eaa47da2
Paul Hamilton <phamtec@mac.com>
2024-08-17 21:02:54 +1000

    Switch back to zeromq 4.1.5 because it has curve

--- dev/zmq.sh
+++ dev/zmq.sh
@@ -5,18 +5,19 @@
 cd working
 [ "$?" != "0" ] && exit 1
 
-wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.19-stable.tar.gz  
-tar xzf libsodium-1.0.19-stable.tar.gz
-cd libsodium-stable
+wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.18.tar.gz
+tar xzf libsodium-1.0.18.tar.gz
+cd libsodium-1.0.18
 ./configure
 make 
 sudo make install
 sudo ldconfig
 cd ..
 
-wget https://github.com/zeromq/libzmq/releases/download/v4.3.5/zeromq-4.3.5.tar.gz
-tar xzf zeromq-4.3.5.tar.gz
-cd zeromq-4.3.5
+# 4.3.5 doesn't seem to have curve? Just use the earlier one for now.
+wget https://github.com/zeromq/zeromq4-1/releases/download/v4.1.5/zeromq-4.1.5.tar.gz
+tar xzf zeromq-4.1.5.tar.gz
+cd zeromq-4.1.5
 ./configure
 make
 sudo make install
