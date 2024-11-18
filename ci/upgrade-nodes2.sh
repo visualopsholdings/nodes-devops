17d2b770f39b35747b444b6fdaba4e61b19bd890
Paul Hamilton <phamtec@mac.com>
2024-11-18 16:27:18 +1100

    Specify tag.

--- ci/upgrade-nodes2.sh
+++ ci/upgrade-nodes2.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 #
 
+if [ "$#" -lt 1 ]; then
+	echo "usage: $0 TAG"
+	exit 1
+fi
+
+TAG=$1
+
 pushd ..
-aws/upgrade-intel.sh nodes2.visualops.com ~/vopsDev/build/awskey-us-west.pem v0.5.0
+aws/upgrade-intel.sh nodes2.visualops.com ~/vopsDev/build/awskey-us-west.pem $TAG
 popd
